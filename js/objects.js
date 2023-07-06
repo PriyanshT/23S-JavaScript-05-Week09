ca6adec1b56c65529baaf529f4c0df3a0f5ceff7
Priyansh Thakar <priyanshthakar@gmail.com>
2023-07-06 19:48:49 -0400

    Objects and new keyword

--- js/objects.js
+++ js/objects.js
@@ -1,9 +1,33 @@
 const output = document.getElementById('output');
 
-/* STEP 1: Create an object (looks a lot like declaring a variable, but with empty braces), 
+/* STEP 1: Create an object (looks a lot like declaring a variable, 
+    but with empty braces), 
 then open this page in a browser and enter 'person' in the console */
+var person = {
+    name: {
+        firstname: "Abc",
+        lastname: "Xyz"
+    },
+    age: 19,
+    gender: "Male",
+    address: {
+        streetNo: 123,
+        street: "Duckworth St",
+        city: "Barrie"
+    },
+    interests: ["programming", "gaming", "singing"],
+    greeting: function () {
+        alert(`Hi! My name is ${this.name.firstname} ${this.name['lastname']}`);
+    },
+    bio: function () {
+        return `The interests of ${this.name.firstname} ${this.name.lastname}, of age ${this.age} living at 
+        ${this.address.streetNo}, ${this.address.street}, ${this.address.city} 
+        are ${this.interests}.`;
+    }
+};
 
-/* STEP 2a: Add other data items to the above object, like name, age, gender, address, and interests */
+/* STEP 2a: Add other data items to the above object, 
+like name, age, gender, address, and interests */
 
 /* STEP 2b: Enter the following directly into the console:
     person.name[0]
@@ -11,42 +35,88 @@ then open this page in a browser and enter 'person' in the console */
     person.gender
     person.interests[0]
 */
+output.textContent = person.name[0];
+output.textContent = typeof (person.age);
+output.textContent = person.gender;
+output.textContent = person.address.streetNo;
+output.textContent = person.interests[0];
 
 /* STEP 3: Add a simple function (now called a method in this context) to the above object 
 called "greeting" that creates an alert dialog, then type person.greeting() into the console */
+console.log(person);
 
-/* STEP 4: Add a more complicated function to the object that describes the person, their age, gender, 
+/* STEP 4: Add a more complicated function to the object that describes 
+the person, their age, gender, 
 and interests in a string that is output to an alert dialog */
+//output.textContent = person.bio();
 
-// The object above is called an 'object literal' - in that we build each part of the object manually, 
-// rather than instatiating it from a class (which we will cover later). Objects can be easier to work with than arrays, as you can name each element
+// The object above is called an 'object literal' - in that we build each 
+// part of the object manually, 
+// rather than instatiating it from a class (which we will cover later). 
+// Objects can be easier to work with than arrays, as you can name each element
 
 /* STEP 5a: Dot syntax allows you to access the parts of an object - the first part is the namespace, 
 and subsequent parts are used to access things encapsulated within the object. Access the name, age, 
 and gender of the person object, and put it inside the <p id="output"></p>. */
+output.textContent = person["name"];
+//output.textContent = person.name;
+output.textContent = person["age"];
 
-/* STEP 5b: Update the above bio() function inside the person object to return the string, instead of the alert(). */
+/* STEP 5b: Update the above bio() function inside the person object to return 
+the string, instead of the alert(). */
 
-/* STEP 6a: An object can even contain another object - update object person above to include a name object. 
-Note how this breaks the greeting and bio functions - update the greeting function to use this sub-namespace */
+/* STEP 6a: An object can even contain another object - u
+pdate object person above to include a name object. 
+Note how this breaks the greeting and bio functions - 
+update the greeting function to use this sub-namespace */
 
 /* STEP 6b: Now fix the bio() function to use the sub-namespace of 'name' as well */
 
 /* STEP 7: You can also use bracket notation, just like an array - 
 objects that are built this way are often referred to as associative arrays */
 
-/* STEP 8: You can also set members of an object - try changing the person's name, and age */
+/* STEP 8: You can also set members of an object - 
+try changing the person's name, and age */
+person.name.firstname = "Robb";
+person.age = 80;
+output.textContent = person.bio();
 
-/* STEP 9a: It it also possible to add new members to an object - include eye color - and type person.eyes in the console */
+/* STEP 9a: It it also possible to add new members to an object - 
+include eye color - and type person.eyes in the console */
+person.eyecolor = "brown";
+console.log(person);
 
-/* STEP 9b: Add a new function called 'goodbye' to the person object and try it by typing person.goodbye into the console */
+/* STEP 9b: Add a new function called 'goodbye' to the person 
+object and try it by typing person.goodbye into the console */
+person.goodbye = function () {
+    alert(`This is ${person.name.firstname} signing off!`);
+};
 
 /* STEP 10: We can also dynamically set both names and values for objects… */
+let newName = "height";
+let newValue = 175;
+person[newName] = newValue;
+console.log(person);
 
 /* STEP 11a: 'this' is very useful - it allows us to refer to the object in question, specifically. 
-This will become much clearer later in the course. Create two new objects, each with a name, and a function that outputs the name */
+This will become much clearer later in the course. Create two new objects, 
+each with a name, and a function that outputs the name */
 
-/* STEP 11b: Call up both person1.hello() and person2.hello() in the console to see how 'this' is specific to each object */
+/* STEP 11b: Call up both person1.hello() and person2.hello() 
+in the console to see how 'this' is specific to each object */
+var person1 = {
+    name: "p1",
+    hello: function () {
+        console.log(`Hi, I am ${this.name}`);
+    }
+};
+
+var person2 = {
+    name: "p2",
+    hello: function () {
+        console.log(`Hi, I am ${this.name}`);
+    }
+};
 
 // You have already been using objects all along:
 let myString = 'Words, words, words.'; // this creates an instance of the class String
--- js/oop.js
+++ js/oop.js
@@ -2,24 +2,64 @@ const output = document.getElementById('output');
 const output2 = document.getElementById('output2');
 
 /* STEP 1a: Create a new object using a regular function */
+function createNewPerson(name) {
+    var obj = {};
+    obj.name = name;
+    obj.greeting = function () {
+        alert(`Hi, I am ${name}`);
+    }
+    return obj;
+}
 
-/* STEP 1b: Use the console to create a new person, and then invoke the function represented by .greeting() */
+/* STEP 1b: Use the console to create a new person, and 
+then invoke the function represented by .greeting() */
+var person1 = createNewPerson("P1");
+var person2 = createNewPerson("P2");
 
 /* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - 
 rewrite the above function, without returning anything. Capitalize the name of the function. */
+// function Person(name) {
+//     this.name = name;
+//     this.greeting = function () {
+//         alert(`Hi, I am ${name}`);
+//     }
+// }
 
 /* STEP 2b: Use the console to create a couple of different people, using the 'new' keyword, 
 and again invoking the .greeting() method for each person */
+// var p1 = new Person("Jon");
+// var p2 = new Person("Jack");
 
 /* STEP 3a: Build the complete constructor for the object Person (comment out the above function first).
  Include name (first and last), age, gender, interests, bio (function), and greeting (function). */
+function Person(firstname, lastname, age, gender, interests) {
+    this.name = {
+        "firstname": firstname,
+        "lastname": lastname
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.greeting = function () {
+        alert(`Hi, I am ${this.name}`);
+    };
+    this.bio = function () {
+        return `The interests of ${this.name.firstname} ${this.name.lastname}, of age ${this.age} living at 
+        are ${this.interests}.`;
+    }
+}
 
 /* STEP 3b: Instantiate a new Person based on the above constructor */
+var p1 = new Person("Abc", "Xyz", 12, "Male", ["programming", "swimming"]);
+var p2 = new Person("Abc2", "Xyz2", 100, "Female", ["swimming", "gaming"]);
 
 /* STEP 3c: Attempt to access the various properties of person1 using the console. */
 // person1['age']
 // person1.interests[1]
 // person1.bio()
+output.textContent = p2["age"];
+output.textContent = p1.interests[1];
+output.textContent = p2.bio();
 
 /* STEP 4a: Alternatively, you can use the Object() constructor to create an object. eg. car*/
 
